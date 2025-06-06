e804afbb6c2a13396debef224da2118e7c1bbf84
Devenified <abhishekpoojary2004@gmail.com>
2025-06-06 04:17:57 -0400

    Added practise set

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,8 @@
 let length;
 let width;
+let groc1;
+let groc2;
+let groc3;
 function calculateArea()
 {
     length=parseFloat(document.getElementById('length').value);
@@ -7,5 +10,13 @@ function calculateArea()
 
     let area=length*width;
 document.getElementById('result').innerText=`The area of rectangle is : ${area}`;
+}
+function calcamt()
+{
+    groc1=parseFloat(documnent.getElementById('grocery1').value);
+    groc2=parseFloat(document.getElementById('grocery2').value);
+    groc3=parseFloat(document.getElementById('grocery3').value);
+    let ans= groc1+groc2+ groc3;
+    document.getElementById('grocresult').innerText=`The total amount is:$ ${ans}`;
 }
 
